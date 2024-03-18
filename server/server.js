ac37953c088589d577462ca3928137af417898ba
Manakin-Wraith <g.mostertpot@gmail.com>
2024-03-18 08:08:12 +0200

    changes loacl server from 3000 to 3001 for server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
